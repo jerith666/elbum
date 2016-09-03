598ee953798887fb117fc7c359d88558f158cebc
Matt McHenry <github@matt.mchenryfamily.org>
2016-09-02 22:21:32 -0400

    key press test code

---
+++ key-test.elm
@@ -0,0 +1,29 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.App exposing (..)
+import Json.Decode as Json
+import Keyboard exposing (..)
+
+main = program { init = init
+               , view = view
+               , update = update
+               , subscriptions = subscriptions
+               }
+
+type alias Model = Int
+
+type Msg = KeyPress Int
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model = case msg of KeyPress k -> (k, Cmd.none)
+
+init : (Model, Cmd Msg)
+init = (0, Cmd.none)
+
+view : Model -> Html Msg
+view model = h1 [ onMouseEnter (KeyPress 1337)
+                , onMouseLeave (KeyPress 1338) ]
+                [ text ("key pressed: " ++ (toString model) ) ]
+
+subscriptions model = presses KeyPress
