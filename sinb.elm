fbad4ad5862ae80c2b2d205bda93cce684e9bb2e
Matt McHenry <github@matt.mchenryfamily.org>
2016-09-23 22:09:17 -0400

    elm-format sinb.elm

--- sinb.elm
+++ sinb.elm
@@ -1,3 +1,5 @@
+module Main exposing (..)
+
 import List exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -7,117 +9,188 @@ import Keyboard exposing (..)
 import Task exposing (..)
 import Http exposing (..)
 import Window exposing (..)
-
 import Album exposing (..)
+
 
 -- MODEL
 
-main = program { init = init
-               , view = view
-               , update = update
-               , subscriptions = subscriptions
-               }
 
-type Msg = Next
-         | Prev
-         | NoUpdate
-         | NoAlbum Http.Error
-         | YesAlbum Album
-         | Resize Size
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
-type alias Model = { album: Maybe Album
-                   , index: Int
-                   }
+
+type Msg
+    = Next
+    | Prev
+    | NoUpdate
+    | NoAlbum Http.Error
+    | YesAlbum Album
+    | Resize Size
+
+
+type alias Model =
+    { album : Maybe Album
+    , index : Int
+    }
+
 
 subscriptions model =
-  Sub.batch [ downs (\keycode -> case keycode of
-                                      39 -> Next {- right arrow -}
-                                      37 -> Prev {- left arrow -}
-                                      _ -> NoUpdate)
-            , resizes Resize
-            ]
+    Sub.batch
+        [ downs
+            (\keycode ->
+                case keycode of
+                    39 ->
+                        -- right arrow
+                        Next
+
+                    37 ->
+                        -- left arrow
+                        Prev
+
+                    _ ->
+                        NoUpdate
+            )
+        , resizes Resize
+        ]
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-       NoAlbum err -> ( model
-                      , Cmd.none
-                      )
-       YesAlbum album -> ( { model
-                           | album = Just album
-                           }
-                         , Cmd.none
-                         )
-       Next -> moveindex model (\i -> i + 1)
-       Prev -> moveindex model (\i -> i - 1)
-       NoUpdate -> moveindex model (\i -> i)
-       Resize size -> ( model, Cmd.none )
+    case msg of
+        NoAlbum err ->
+            ( model
+            , Cmd.none
+            )
+
+        YesAlbum album ->
+            ( { model
+                | album = Just album
+              }
+            , Cmd.none
+            )
+
+        Next ->
+            moveindex model (\i -> i + 1)
+
+        Prev ->
+            moveindex model (\i -> i - 1)
+
+        NoUpdate ->
+            moveindex model (\i -> i)
+
+        Resize size ->
+            ( model, Cmd.none )
+
 
 moveindex model mover =
-  ( case model.album of
-         Nothing -> model
-         Just a -> let newi = mover model.index
-                   in { model
-                      | index = Basics.min (Basics.max newi
-                                                       0)
-                                           (length a.images - 1)
-                      }
-  , Cmd.none
-  )
+    ( case model.album of
+        Nothing ->
+            model
 
-init = ( { album = Nothing
-         , index = 0
-         }
-       , Task.perform NoAlbum YesAlbum (Http.get jsonDecAlbum "album.json")
-       )
+        Just a ->
+            let
+                newi =
+                    mover model.index
+            in
+                { model
+                    | index =
+                        Basics.min
+                            (Basics.max newi
+                                0
+                            )
+                            (length a.images - 1)
+                }
+    , Cmd.none
+    )
+
+
+init =
+    ( { album = Nothing
+      , index = 0
+      }
+    , Task.perform NoAlbum YesAlbum (Http.get jsonDecAlbum "album.json")
+    )
+
 
 view : Model -> Html Msg
 view model =
-  case model.album of
-       Nothing -> div [] [ text "album loading ..." ]
-       Just a ->
-         div []
-             [ h1 [] [ text a.title ]
-             , renderImgs a.images model.index
-             ]
+    case model.album of
+        Nothing ->
+            div [] [ text "album loading ..." ]
+
+        Just a ->
+            div []
+                [ h1 [] [ text a.title ]
+                , renderImgs a.images model.index
+                ]
+
 
 renderImgs : List Image -> Int -> Html Msg
 renderImgs imgs index =
-  div []
-      ( [ renderMainImage ( head ( drop index imgs ) ) ]
-        ++ [ br [] [] ] ++
-        renderThumbs imgs index
-      )
+    div []
+        ([ renderMainImage (head (drop index imgs)) ]
+            ++ [ br [] [] ]
+            ++ renderThumbs imgs index
+        )
+
 
 renderMainImage : Maybe Image -> Html Msg
 renderMainImage img =
-  case img of
-       Nothing -> div [] [text "no images in album"]
-       Just i -> renderImg i
+    case img of
+        Nothing ->
+            div [] [ text "no images in album" ]
+
+        Just i ->
+            renderImg i
+
 
 renderThumbs imgs index =
-  List.map renderThumb imgs
+    List.map renderThumb imgs
+
 
 renderImg : Image -> Html Msg
-renderImg ises = case ises.srcSet.srcs of
-                      [] -> div [] []
-                      is1::_ -> render is1 Next
+renderImg ises =
+    case ises.srcSet.srcs of
+        [] ->
+            div [] []
+
+        is1 :: _ ->
+            render is1 Next
+
 
 renderThumb : Image -> Html Msg
-renderThumb ises = case ises.srcSet.srcs of
-                        [] -> div [] []
-                        is1::_ -> render ( thumbScale is1 ) Prev
+renderThumb ises =
+    case ises.srcSet.srcs of
+        [] ->
+            div [] []
+
+        is1 :: _ ->
+            render (thumbScale is1) Prev
+
 
 thumbScale : ImgSrc -> ImgSrc
-thumbScale i = { i | x = tScale i.x
-                   , y = tScale i.y
-                   }
+thumbScale i =
+    { i
+        | x = tScale i.x
+        , y = tScale i.y
+    }
 
-tScale x = round (toFloat x * 0.2)
+
+tScale x =
+    round (toFloat x * 0.2)
+
 
 render : ImgSrc -> Msg -> Html Msg
-render i msg = img [ src i.url
-                   , Html.Attributes.width i.x
-                   , Html.Attributes.height i.y
-                   , onClick msg ]
-                   []
+render i msg =
+    img
+        [ src i.url
+        , Html.Attributes.width i.x
+        , Html.Attributes.height i.y
+        , onClick msg
+        ]
+        []
