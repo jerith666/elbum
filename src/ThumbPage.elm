69be58cd72fb5c3c4720e3b5a38bba61654683f8
Matt McHenry <github@matt.mchenryfamily.org>
2017-01-02 21:48:20 -0500

    begin to sketch out ThumbPage view

--- src/ThumbPage.elm
+++ src/ThumbPage.elm
@@ -4,6 +4,9 @@ import Album exposing (..)
 import WinSize exposing (..)
 
 import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+import Css exposing (..)
 
 type alias ThumbPageModel =
     { album : Album
@@ -12,5 +15,50 @@ type alias ThumbPageModel =
 
 view : (Int -> msg) -> ThumbPageModel -> Html msg
 view imgChosenMsgr thumbPageModel =
-    div [] [ Html.text ("Thumb Page for " ++ thumbPageModel.album.title ++ " at " ++ (toString thumbPageModel.winSize.width) ++ "x" ++ (toString thumbPageModel.winSize.height)) ]
+    rootDivFlexCol
+        [ backgroundColor black ]
+        <| viewThumbs 0 imgChosenMsgr thumbPageModel
+
+viewThumbs : Int -> (Int -> msg) -> ThumbPageModel -> List (Html msg)
+viewThumbs i imgChosenMsgr thumbPageModel =
+    case List.head <| List.drop i thumbPageModel.album.images of
+        Just nextImg ->
+            viewThumb (imgChosenMsgr i) nextImg
+            :: viewThumbs (i+1) imgChosenMsgr thumbPageModel
+
+        Nothing ->
+            []
+
+
+viewThumb : msg -> Image -> Html msg
+viewThumb selectedMsg img = Html.text "TODO view"
+
+-- TODO move below to utils file
+
+black =
+    rgb 0 0 0
+
+styles =
+    Css.asPairs >> Html.Attributes.style
+
+rootDiv extraStyles =
+    div
+        [ styles <|
+            [ position absolute
+            , Css.height (vh 100)
+            , Css.width (vw 100)
+            -- , overflow auto
+            , backgroundColor black
+            ]
+            ++ extraStyles
+        ]
+
+rootDivFlexCol extraStyles =
+    rootDiv <|
+        [ displayFlex
+        , flexDirection column
+        ]
+        ++ extraStyles
+
+-- div [onClick (imgChosenMsgr 1)] [ Html.text ("Thumb Page for " ++ thumbPageModel.album.title ++ " at " ++ (toString thumbPageModel.winSize.width) ++ "x" ++ (toString thumbPageModel.winSize.height)) ]
 
