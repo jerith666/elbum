f6d268842331966ffc65d08ed91ddcbb454d88b1
Matt McHenry <github@matt.mchenryfamily.org>
2017-01-03 23:29:06 -0500

    Revert "lower maxThumbWidth back to 400"
    This reverts commit e2c603fc292ce6677a57b9e75c1a32a9520d7503.

--- src/ThumbPage.elm
+++ src/ThumbPage.elm
@@ -13,7 +13,7 @@ type alias ThumbPageModel =
     , winSize : WinSize
     }
 
-maxThumbWidth = 400
+maxThumbWidth = 600
 
 view : (Int -> msg) -> ThumbPageModel -> Html msg
 view imgChosenMsgr thumbPageModel =
