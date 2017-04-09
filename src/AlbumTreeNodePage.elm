7221b4e1ffeba3190554b5e81430c0116a766c89
Matt McHenry <github@matt.mchenryfamily.org>
2017-04-09 17:09:26 -0400

    stub impl of links to subtrees and leaf albums

--- src/AlbumTreeNodePage.elm
+++ src/AlbumTreeNodePage.elm
@@ -1,7 +1,10 @@
 module AlbumTreeNodePage exposing (AlbumTreeNodePage(..), AlbumTreeNodePageMsg, view)
 
 import Html exposing (..)
+import Css exposing (..)
+
 import Album exposing (..)
+import AlbumStyles exposing (..)
 import WinSize exposing (..)
 
 
@@ -14,4 +17,30 @@ type AlbumTreeNodePageMsg
 
 
 view (AlbumTreeNodePage albumTreeNode winSize parent) =
-    Html.text <| "album tree node page for " ++ albumTreeNode.nodeTitle
+    rootDivFlex
+        column
+        []
+        <| [ viewTitle albumTreeNode.nodeTitle ]
+        ++ [ viewChildNode albumTreeNode.childFirst ]
+        ++ List.map viewChildNode albumTreeNode.childRest
+
+
+viewTitle title =
+    div [styles [color white
+                 , textAlign center
+                 , Css.width (vw 100)
+                 , padding (px 5)
+                 ]]
+        [ Html.text <| "album tree node page for " ++ title ]
+
+
+viewChildNode nodeOrAlbum =
+    case nodeOrAlbum of
+        Subtree albumTreeNode ->
+            div
+                [ styles [ color white ] ]
+                [ Html.text <| "link to sub album " ++ albumTreeNode.nodeTitle ]
+        Leaf album ->
+            div
+                [ styles [ color white ] ]
+                [ Html.text <| "link to leaf album " ++ album.title ]
