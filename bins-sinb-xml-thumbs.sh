7c786cb037e50db0e94c1859336ab87e1f8cfba9
Matt McHenry <github@matt.mchenryfamily.org>
2017-10-29 10:10:28 -0400

    script to convert bins album.xml to sinb thumbnail symlinks

---
+++ bins-sinb-xml-thumbs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit;
+set -o nounset;
+
+find . -name album.xml | while read a; do
+    t=$(xmllint --xpath "/album/description/field/text()" "$a" | sed 's/^[[:space:]]*//g' | tr -d '\n');
+    (cd "$(dirname "$a")";
+     pwd;
+     ln -s "${t}" thumbnail;)
+done
