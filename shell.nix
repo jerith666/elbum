9f7c431ae1f0850e53a1cb4e35935f4da661d4b2
Matt McHenry <github@matt.mchenryfamily.org>
2017-11-26 22:47:30 -0500

    replace kludgy nix-shell.sh with proper shell.nix

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ nixpkgs ? import <nixpkgs> {}}: #, compiler ? "ghc7102" }:
+let
+  inherit (nixpkgs) pkgs;
+  #ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
+  ghc = pkgs.haskellPackages.ghcWithPackages (ps: with ps; [
+          friday-juicypixels elm-bridge friday directory_1_3_1_5 regex-compat
+        ]);
+in
+pkgs.stdenv.mkDerivation {
+  name = "elbum-haskell-env-0";
+  buildInputs = [ ghc pkgs.elmPackages.elm ];
+  shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
+}
