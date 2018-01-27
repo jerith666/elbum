728e321e4a9586a2d4be962eba6d2a4b81b59a45
Matt McHenry <github@matt.mchenryfamily.org>
2018-01-26 21:47:03 -0500

    implement ProgressiveImage

---
+++ src/client/ImgProgressTest.elm
@@ -0,0 +1,73 @@
+module ImgProgressTest exposing (..)
+
+--import Css exposing (..)
+--import Html.Attributes exposing (..)
+--import Html.Events exposing (..)
+--import Json.Decode exposing (..)
+
+import Album exposing (..)
+import Html exposing (program)
+import Html.Styled exposing (..)
+import ProgressiveImage exposing (..)
+
+
+type Model
+    = Mo ProgressiveImageModel
+
+
+type Msg
+    = Me ProgressiveImageMsg
+
+
+main : Program Never Model Msg
+main =
+    Html.program { init = init, view = view >> toUnstyled, update = update, subscriptions = subscriptions }
+
+
+mainImg : ImgSrc
+mainImg =
+    ImgSrc "10_October 06-09: Ohiopyle Weekend/DSC_1495.JPG" 1600 1069
+
+
+fallback : ImgSrc
+fallback =
+    ImgSrc "10_October 06-09: Ohiopyle Weekend/DSC_1495.200.png" 200 133
+
+
+cached : ImgSrc
+cached =
+    ImgSrc "10_October 06-09: Ohiopyle Weekend/DSC_1495.800.png" 800 534
+
+
+init : ( Model, Cmd Msg )
+init =
+    let
+        ( model, cmd ) =
+            ProgressiveImage.init
+                { fallback = fallback
+                , possiblyCached = [ cached ]
+                , mainImg = mainImg
+                , width = 1600
+                , height = 1069
+                }
+    in
+    ( Mo model, Cmd.map Me cmd )
+
+
+view : Model -> Html Msg
+view (Mo model) =
+    Html.Styled.map Me <| ProgressiveImage.view model
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update (Me msg) (Mo model) =
+    let
+        ( newModel, newCmd ) =
+            ProgressiveImage.update msg model
+    in
+    ( Mo newModel, Cmd.map Me newCmd )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions (Mo model) =
+    Sub.map Me <| ProgressiveImage.subscriptions model
---
+++ src/client/ProgressiveImage.elm
@@ -0,0 +1,295 @@
+module ProgressiveImage exposing (ProgressiveImageData, ProgressiveImageModel, ProgressiveImageMsg, init, subscriptions, update, updateCmd, view)
+
+import Album exposing (ImgSrc)
+import AlbumStyles exposing (..)
+import Animation exposing (px)
+import Animation.Messenger exposing (..)
+import Css exposing (..)
+import Delay exposing (..)
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (..)
+import Html.Styled.Events exposing (..)
+import ImageViews exposing (..)
+import Json.Decode exposing (..)
+import Time exposing (..)
+
+
+type alias ProgressiveImageData =
+    { fallback : ImgSrc
+    , possiblyCached : List ImgSrc
+    , mainImg : ImgSrc
+    , width : Int
+    , height : Int
+    }
+
+
+{-| control flow:
+
+1.  try some images that might already be cached.
+    leave these images hidden while we load them, but timeout quickly if they don't show up.
+    if one does show up, skip the fallback.
+
+2.  if none of the cached images loads, load a fallback image.
+    TODO maybe let it be visible from the get-go, as on really slow connections even this may take some time?
+
+3.  once we have some placeholder image loaded, fade it in with a CSS animation -- this requires carefully choreographed DOM updates.
+    meanwhile, begin loading the main image. keep it hidden while it loads.
+
+4.  once the main image is loaded, cross-fade from the placeholder to the main image.
+    again, this requires careful choreography.
+
+-}
+type ProgressiveImageStatus
+    = TryingCached (List ImgSrc) ImgSrc (List ImgSrc)
+    | LoadingFallback
+    | LoadingMain ImgSrc
+    | MainLoaded ImgSrc
+    | MainOnly
+
+
+type alias AnimState =
+    { main : Animation.Messenger.State ProgressiveImageMsg
+    , placeholder : Animation.State
+    }
+
+
+type ProgressiveImageModel
+    = ProgImgModel ProgressiveImageData ProgressiveImageStatus AnimState
+
+
+type ProgressiveImageMsg
+    = Loaded ImgSrc
+    | Timeout ImgSrc
+    | MainFadeinComplete
+    | AnimatePlaceholder Animation.Msg
+    | AnimateMain Animation.Msg
+
+
+hidden : List Animation.Property
+hidden =
+    [ Animation.opacity 0 ]
+
+
+hide : Animation.State -> Animation.State
+hide =
+    Animation.interrupt [ Animation.to hidden ]
+
+
+shown : List Animation.Property
+shown =
+    [ Animation.opacity 1 ]
+
+
+show : Animation.State -> Animation.State
+show =
+    Animation.interrupt [ Animation.to shown ]
+
+
+showMsg : Animation.Messenger.State ProgressiveImageMsg -> Animation.Messenger.State ProgressiveImageMsg
+showMsg =
+    Animation.interrupt [ Animation.to shown, Animation.Messenger.send MainFadeinComplete ]
+
+
+init : ProgressiveImageData -> ( ProgressiveImageModel, Cmd ProgressiveImageMsg )
+init data =
+    let
+        animState =
+            { main = Animation.style hidden, placeholder = Animation.style hidden }
+
+        model =
+            case data.possiblyCached of
+                [] ->
+                    Debug.log "start with fallback" <| ProgImgModel data LoadingFallback animState
+
+                c1 :: cOthers ->
+                    Debug.log "start with cached" <| ProgImgModel data (TryingCached [] c1 cOthers) animState
+    in
+    ( model, updateCmd model )
+
+
+update : ProgressiveImageMsg -> ProgressiveImageModel -> ( ProgressiveImageModel, Cmd ProgressiveImageMsg )
+update msg ((ProgImgModel data status animState) as oldModel) =
+    case msg of
+        AnimateMain animMsg ->
+            let
+                ( newMainState, animCmd ) =
+                    Animation.Messenger.update animMsg animState.main
+            in
+            ( ProgImgModel data status <| { animState | main = newMainState }, animCmd )
+
+        _ ->
+            let
+                newModel =
+                    updateModel msg oldModel
+            in
+            ( newModel, updateCmd newModel )
+
+
+updateModel : ProgressiveImageMsg -> ProgressiveImageModel -> ProgressiveImageModel
+updateModel msg ((ProgImgModel data status animState) as model) =
+    case msg of
+        Loaded imgSrc ->
+            Debug.log "new model after Loaded" <|
+                case Debug.log ("loaded " ++ imgSrc.url ++ " in status") status of
+                    TryingCached tried trying upnext ->
+                        if imgSrc == trying then
+                            ProgImgModel data (LoadingMain trying) { animState | placeholder = show animState.placeholder }
+                        else
+                            --maybe some earlier tried image?  ignore
+                            model
+
+                    LoadingFallback ->
+                        if imgSrc == data.fallback then
+                            ProgImgModel data (LoadingMain data.fallback) { animState | placeholder = show animState.placeholder }
+                        else
+                            --maybe some earlier tryingCached?  ignore
+                            model
+
+                    LoadingMain placeholder ->
+                        if imgSrc == data.mainImg then
+                            ProgImgModel data (MainLoaded placeholder) { animState | main = showMsg animState.main }
+                        else
+                            --something stale, ignore
+                            model
+
+                    MainLoaded _ ->
+                        --some stale loading notification, ignore
+                        model
+
+                    MainOnly ->
+                        --some stale loading notification, ignore
+                        model
+
+        Timeout imgSrc ->
+            Debug.log "new model after Timeout" <|
+                case Debug.log ("timeout " ++ imgSrc.url ++ " in status") status of
+                    TryingCached tried trying upnext ->
+                        case upnext of
+                            [] ->
+                                Debug.log "timeout to fallback" <| ProgImgModel data LoadingFallback animState
+
+                            next :: later ->
+                                Debug.log "timeout to next cached" <| ProgImgModel data (TryingCached (tried ++ [ trying ]) next later) animState
+
+                    LoadingFallback ->
+                        --shouldn't happen
+                        model
+
+                    LoadingMain _ ->
+                        -- shouldn't happen
+                        model
+
+                    MainLoaded _ ->
+                        --shouldn't happen
+                        model
+
+                    MainOnly ->
+                        --shouldn't happen
+                        model
+
+        MainFadeinComplete ->
+            Debug.log "new model after MainFadeinComplete" <|
+                case Debug.log "main fadein complete in status" status of
+                    MainLoaded _ ->
+                        ProgImgModel data MainOnly { animState | placeholder = hide animState.placeholder }
+
+                    TryingCached _ _ _ ->
+                        --shouldn't happen
+                        model
+
+                    LoadingFallback ->
+                        --shouldn't happen
+                        model
+
+                    LoadingMain _ ->
+                        --shouldn't happen
+                        model
+
+                    MainOnly ->
+                        --shouldn't happen
+                        model
+
+        AnimateMain animMsg ->
+            --taken care of by caller, can't happen
+            model
+
+        AnimatePlaceholder animMsg ->
+            ProgImgModel data status <| { animState | placeholder = Animation.update animMsg animState.placeholder }
+
+
+updateCmd : ProgressiveImageModel -> Cmd ProgressiveImageMsg
+updateCmd (ProgImgModel data status animState) =
+    case status of
+        TryingCached _ trying _ ->
+            Delay.after 200 millisecond <| Timeout <| Debug.log "starting 200 ms timeout for " trying
+
+        LoadingFallback ->
+            Cmd.none
+
+        LoadingMain _ ->
+            Cmd.none
+
+        MainLoaded oldPlaceholder ->
+            Cmd.none
+
+        MainOnly ->
+            Cmd.none
+
+
+subscriptions : ProgressiveImageModel -> Sub ProgressiveImageMsg
+subscriptions (ProgImgModel data status animState) =
+    Sub.batch
+        [ Animation.subscription AnimatePlaceholder [ animState.placeholder ]
+        , Animation.subscription AnimateMain [ animState.main ]
+        ]
+
+
+view : ProgressiveImageModel -> Html ProgressiveImageMsg
+view (ProgImgModel data status animState) =
+    case status of
+        TryingCached tried trying nextup ->
+            viewImg trying data (styledAnimation animState.placeholder) []
+
+        LoadingFallback ->
+            viewImg data.fallback data (styledAnimation animState.placeholder) []
+
+        LoadingMain placeholder ->
+            viewLoadingMain data placeholder animState.placeholder animState.main
+
+        MainLoaded oldPlaceholder ->
+            viewLoadingMain data oldPlaceholder animState.placeholder animState.main
+
+        MainOnly ->
+            viewImg data.mainImg data (styledMsgAnimation animState.main) []
+
+
+viewLoadingMain : ProgressiveImageData -> ImgSrc -> Animation.State -> Animation.Messenger.State ProgressiveImageMsg -> Html ProgressiveImageMsg
+viewLoadingMain data imgSrc imgSrcAnimState mainAnimState =
+    div
+        [ styles [ position relative ] ]
+        [ viewImg data.mainImg data (styledMsgAnimation mainAnimState) [ position absolute, Css.top zero, Css.left zero, zIndex (Css.int 1) ]
+        , viewImg imgSrc data (styledAnimation imgSrcAnimState) []
+        ]
+
+
+viewImg : ImgSrc -> ProgressiveImageData -> List (Attribute ProgressiveImageMsg) -> List Style -> Html ProgressiveImageMsg
+viewImg imgSrc data animStyle styles =
+    renderPresized
+        0
+        data.width
+        data.height
+        imgSrc
+        []
+        styles
+        (Debug.log ("style for " ++ imgSrc.url) animStyle ++ [ on "load" <| succeed <| Loaded imgSrc ])
+        Nothing
+
+
+styledAnimation : Animation.State -> List (Attribute ProgressiveImageMsg)
+styledAnimation animState =
+    List.map Html.Styled.Attributes.fromUnstyled (Animation.render animState)
+
+
+styledMsgAnimation : Animation.Messenger.State ProgressiveImageMsg -> List (Attribute ProgressiveImageMsg)
+styledMsgAnimation animState =
+    List.map Html.Styled.Attributes.fromUnstyled (Animation.render animState)
