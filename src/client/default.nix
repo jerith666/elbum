7db18aca313da45a1972a5304f0f509e2ec28879
Matt McHenry <github@matt.mchenryfamily.org>
2018-03-01 22:05:30 -0500

    as generated by elm2nix

---
+++ src/client/default.nix
@@ -0,0 +1,58 @@
+{ nixpkgs ? <nixpkgs>
+, config ? {} }:
+
+with (import nixpkgs config);
+
+let
+  mkDerivation =
+    { srcs ? ./elm-srcs.nix
+    , src
+    , name
+    , srcdir ? "./src"
+    , targets ? []
+    }:
+    let
+      sources = import srcs { inherit fetchzip; };
+      exactDependencies = builtins.toFile "exact-dependencies.json"
+        (builtins.toJSON (lib.mapAttrs (name: value: value.version) sources));
+    in stdenv.mkDerivation {
+      inherit name src;
+
+      buildInputs = [ elmPackages.elm ];
+
+      preConfigurePhases = ["setupElmStuffPhase"];
+
+      setupElmStuffPhase = ''
+        runHook preSetupElmStuffPhase
+
+        rm -rf elm-stuff
+        mkdir elm-stuff
+        ln -s ${exactDependencies} elm-stuff/exact-dependencies.json
+
+        ${lib.concatStrings (lib.mapAttrsToList (name: src: ''
+          mkdir -p elm-stuff/packages/${name}
+          ln -s ${src.src} elm-stuff/packages/${name}/${src.version}
+        '') sources)}
+
+        runHook postSetupElmStuffPhase
+      '';
+
+      buildPhase = let
+        elmfile = module: "${srcdir}/${builtins.replaceStrings ["."] ["/"] module}.elm";
+      in ''
+        mkdir -p $out/share/doc
+        ${lib.concatStrings (map (module: ''
+          elm make --warn ${elmfile module} --output $out/${module}.js --docs $out/share/doc/${module}.json
+        '') targets)}
+      '';
+
+      installPhase = ":";
+    };
+in mkDerivation {
+  name = "jerith666-elbum-0.1.0";
+  srcs = ./elm-srcs.nix;
+  src = ./.;
+  srcdir = ".";
+  targets = [];
+}
+
---
+++ src/client/elm-srcs.nix
@@ -0,0 +1,170 @@
+{ fetchzip }: {
+  "Bogdanp/elm-combine" = {
+    src = fetchzip {
+      url = "https://github.com/Bogdanp/elm-combine/archive/3.1.1.zip";
+      sha256 = "0cpd50qab54hlnc1c3pf2j7j45cr3y4nhkf53p8d1w7rm7kyqssb";
+    };
+    version = "3.1.1";
+  };
+  "Skinney/murmur3" = {
+    src = fetchzip {
+      url = "https://github.com/Skinney/murmur3/archive/2.0.6.zip";
+      sha256 = "1fsb30871fhav809kmmj5zd2lwln10lpz8112l8dxhw301rk0nb1";
+    };
+    version = "2.0.6";
+  };
+  "andrewMacmurray/elm-delay" = {
+    src = fetchzip {
+      url = "https://github.com/andrewMacmurray/elm-delay/archive/2.0.3.zip";
+      sha256 = "0cznf7f4kls3x23qnlmmard35lnppgim1qcy2nyag4a684srn358";
+    };
+    version = "2.0.3";
+  };
+  "bartavelle/json-helpers" = {
+    src = fetchzip {
+      url = "https://github.com/bartavelle/json-helpers/archive/1.4.0.zip";
+      sha256 = "13wc7vxdcf3ap33xsgvl63d20791380whvk032p1iyg5wfapc7p5";
+    };
+    version = "1.4.0";
+  };
+  "ccapndave/elm-update-extra" = {
+    src = fetchzip {
+      url = "https://github.com/ccapndave/elm-update-extra/archive/3.0.0.zip";
+      sha256 = "177y4hyl0wbxxlkhm15nih9wz0pr28xwnd51gm0yb5q6vnnyjh6q";
+    };
+    version = "3.0.0";
+  };
+  "elm-community/basics-extra" = {
+    src = fetchzip {
+      url = "https://github.com/elm-community/basics-extra/archive/2.2.0.zip";
+      sha256 = "0q4pw7wwsf13gakzyxqagy6v5dj6ch0n9mhx2slq8w1l19mf1z0i";
+    };
+    version = "2.2.0";
+  };
+  "elm-lang/animation-frame" = {
+    src = fetchzip {
+      url = "https://github.com/elm-lang/animation-frame/archive/1.0.1.zip";
+      sha256 = "1rlhpz64sh2f3l9bq9h2qqgpvcqz661ynda4l13fbqpxxfcdcpy2";
+    };
+    version = "1.0.1";
+  };
+  "elm-lang/core" = {
+    src = fetchzip {
+      url = "https://github.com/elm-lang/core/archive/5.1.1.zip";
+      sha256 = "0iww5kfxwymwj1748q0i629vyr1yjdqsx1fvymra6866gr2m3n19";
+    };
+    version = "5.1.1";
+  };
+  "elm-lang/dom" = {
+    src = fetchzip {
+      url = "https://github.com/elm-lang/dom/archive/1.1.1.zip";
+      sha256 = "181yams19hf391dgvjxvamfxdkh49i83qfdwj9big243ljd08mpv";
+    };
+    version = "1.1.1";
+  };
+  "elm-lang/html" = {
+    src = fetchzip {
+      url = "https://github.com/elm-lang/html/archive/2.0.0.zip";
+      sha256 = "08mxkcb1548fcvc1p7r3g1ycrdwmfkyma24jf72238lf7j4ps4ng";
+    };
+    version = "2.0.0";
+  };
+  "elm-lang/http" = {
+    src = fetchzip {
+      url = "https://github.com/elm-lang/http/archive/1.0.0.zip";
+      sha256 = "1bhhh81ih7psvs324i5cdac5w79nc9jmykm76cmy6awfp7zhqb4b";
+    };
+    version = "1.0.0";
+  };
+  "elm-lang/keyboard" = {
+    src = fetchzip {
+      url = "https://github.com/elm-lang/keyboard/archive/1.0.1.zip";
+      sha256 = "14cd450j44him2cj523kj1fnq843sxkidqdxjs8hkf6vqzvrc6sy";
+    };
+    version = "1.0.1";
+  };
+  "elm-lang/lazy" = {
+    src = fetchzip {
+      url = "https://github.com/elm-lang/lazy/archive/2.0.0.zip";
+      sha256 = "09439a2pkzbwvk2lfrg229jwjvc4d5dc32562q276z012bw1a2lc";
+    };
+    version = "2.0.0";
+  };
+  "elm-lang/navigation" = {
+    src = fetchzip {
+      url = "https://github.com/elm-lang/navigation/archive/2.1.0.zip";
+      sha256 = "08rb6b740yxz5jpgkf5qad1raf7qqnx77hzn3bkk0s4kqyirhfcl";
+    };
+    version = "2.1.0";
+  };
+  "elm-lang/svg" = {
+    src = fetchzip {
+      url = "https://github.com/elm-lang/svg/archive/2.0.0.zip";
+      sha256 = "1asb89d2y554ma84dnd0x7a9g29nzdv355bji91s1zrajr96gcc5";
+    };
+    version = "2.0.0";
+  };
+  "elm-lang/virtual-dom" = {
+    src = fetchzip {
+      url = "https://github.com/elm-lang/virtual-dom/archive/2.0.4.zip";
+      sha256 = "1zydzzhpxivpzddnxjsjlwc18kaya1l4f8zsgs22wkq25izkin33";
+    };
+    version = "2.0.4";
+  };
+  "elm-lang/window" = {
+    src = fetchzip {
+      url = "https://github.com/elm-lang/window/archive/1.0.1.zip";
+      sha256 = "1w6s8bqpwy2nkg70dflijl9jzamybqrs5iv9f537pw6r8fl8dr34";
+    };
+    version = "1.0.1";
+  };
+  "knledg/touch-events" = {
+    src = fetchzip {
+      url = "https://github.com/knledg/touch-events/archive/2.1.2.zip";
+      sha256 = "0c4by39abgd3pscj9c2wj0fawm5cqvwphmw9w16fha8i3fcl5l43";
+    };
+    version = "2.1.2";
+  };
+  "mdgriffith/elm-style-animation" = {
+    src = fetchzip {
+      url = "https://github.com/mdgriffith/elm-style-animation/archive/3.5.5.zip";
+      sha256 = "0m3n0xa5bqlwnd0235x145h1crcr3vv507i3j8457xq2550d3c7d";
+    };
+    version = "3.5.5";
+  };
+  "rgrempel/elm-route-url" = {
+    src = fetchzip {
+      url = "https://github.com/rgrempel/elm-route-url/archive/4.0.0.zip";
+      sha256 = "11af0f0s1a70jaqp9n673fji1sahsa87x47c21yck1gix17ppzy5";
+    };
+    version = "4.0.0";
+  };
+  "rtfeldman/elm-css" = {
+    src = fetchzip {
+      url = "https://github.com/rtfeldman/elm-css/archive/13.1.1.zip";
+      sha256 = "1w7bcdqvwmzjajk7b9jassipkaxpdz4b04g4hyf5c9hknl47qf1i";
+    };
+    version = "13.1.1";
+  };
+  "rtfeldman/elm-css-util" = {
+    src = fetchzip {
+      url = "https://github.com/rtfeldman/elm-css-util/archive/1.0.2.zip";
+      sha256 = "1ng4k7gs09qllxhi7injj8n148jl745h4y4gn54fgdw9m87gp6yi";
+    };
+    version = "1.0.2";
+  };
+  "rtfeldman/hex" = {
+    src = fetchzip {
+      url = "https://github.com/rtfeldman/hex/archive/1.0.0.zip";
+      sha256 = "1lvc6kz5y976gi469a2c3d9bzjbjb6pf8a4qdzgq680wfw7y1hz7";
+    };
+    version = "1.0.0";
+  };
+  "sporto/erl" = {
+    src = fetchzip {
+      url = "https://github.com/sporto/erl/archive/13.0.2.zip";
+      sha256 = "11fcxk3f61ikccglrl60xwgp3zzhr595ypj4anpczv47q9li930z";
+    };
+    version = "13.0.2";
+  };
+}
