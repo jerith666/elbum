2023c77505531d92ec1571c67f334c9e2f050a25
Matt McHenry <github@matt.mchenryfamily.org>
2018-06-20 21:04:00 -0400

    add fallback to default thumbnails in bins util script

--- src/utils/bins-sinb-xml-thumbs.sh
+++ src/utils/bins-sinb-xml-thumbs.sh
@@ -3,8 +3,14 @@
 set -o errexit;
 set -o nounset;
 
-find . -name album.xml | while read a; do
+find . -name album.xml | sort | while read a; do
     t=$(xmllint --xpath "/album/description/field[@name='sampleimage']/text()" "$a" | sed 's/^[[:space:]]*//g' | tr -d '\n');
+    if [ -z "${t}" ]; then
+        t="$(find "${a/album.xml/}" -iname *.jpg -or -iname *.jpeg -or -iname *.png | head -1)";
+        t=$(basename "$t");
+        echo "falling back to default thumbnail $t";
+    fi
+    
     (cd "$(dirname "$a")";
      pwd;
      ln -sf "${t}" thumbnail;)
