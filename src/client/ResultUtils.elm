c0d90cd15d97a07aa2b2664f924d1300db423ef7
Matt McHenry <github@matt.mchenryfamily.org>
2018-07-27 21:42:51 -0400

    abortive serialize attempt

--- src/client/ResultUtils.elm
+++ src/client/ResultUtils.elm
@@ -14,6 +14,23 @@ either errMapper okMapper r =
             errMapper error
 
 
+serialize : List a -> (a -> a -> a) -> a -> Cmd a
+serialize msgs reducer id =
+    Task.perform identity <|
+        Task.map (List.foldl reducer id) <|
+            s2 msgs
+
+
+s2 : List a -> Task Never (List a)
+s2 msgs =
+    Task.sequence <| s1 msgs
+
+
+s1 : List a -> List (Task Never a)
+s1 msgs =
+    List.map (\m -> Task.succeed m) msgs
+
+
 toCmd : a -> Cmd a
 toCmd m =
     Task.perform identity <| Task.succeed m
