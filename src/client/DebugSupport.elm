5c6ff7543e707497268bf670dd2eaa19c118db94
Matt McHenry <github@matt.mchenryfamily.org>
2018-11-18 12:01:29 -0500

    disable debugging

--- src/client/DebugSupport.elm
+++ src/client/DebugSupport.elm
@@ -3,11 +3,11 @@ module DebugSupport exposing (debugString, log)
 
 log : String -> a -> a
 log str val =
-    --val
-    Debug.log str val
+    --Debug.log str val
+    val
 
 
 debugString : a -> String
 debugString val =
-    --"Debug.toString not supported in --optimize mode"
-    Debug.toString val
+    --Debug.toString val
+    "Debug.toString not supported in --optimize mode"
