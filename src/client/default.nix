04faada8c6bc035e4435689a1e0fea6fa97ee59c
Matt McHenry <github@matt.mchenryfamily.org>
2018-11-18 15:01:57 -0500

    update nix expression for 0.19

--- src/client/default.nix
+++ src/client/default.nix
@@ -13,31 +13,15 @@ let
     , srcdir ? "./src"
     , targets ? []
     }:
-    let
-      sources = import srcs { inherit fetchzip; };
-      exactDependencies = builtins.toFile "exact-dependencies.json"
-        (builtins.toJSON (lib.mapAttrs (name: value: value.version) sources));
-    in stdenv.mkDerivation {
+    stdenv.mkDerivation {
       inherit name src;
 
       buildInputs = [ elmPackages.elm ];
 
-      preConfigurePhases = ["setupElmStuffPhase"];
-
-      setupElmStuffPhase = ''
-        runHook preSetupElmStuffPhase
-
-        rm -rf elm-stuff
-        mkdir elm-stuff
-        ln -s ${exactDependencies} elm-stuff/exact-dependencies.json
-
-        ${lib.concatStrings (lib.mapAttrsToList (name: src: ''
-          mkdir -p elm-stuff/packages/${name}
-          ln -s ${src.src} elm-stuff/packages/${name}/${src.version}
-        '') sources)}
-
-        runHook postSetupElmStuffPhase
-      '';
+      configurePhase = (elmPackages.fetchElmDeps {
+        elmPackages = import ./elm-srcs.nix;
+        versionsDat = ./versions.dat;
+      });
 
       buildPhase = let
         elmfile = module: "${srcdir}/${builtins.replaceStrings ["."] ["/"] module}.elm";
@@ -45,7 +29,7 @@ let
         mkdir -p $out/share/doc
         cp -iv ${albumTypes}/Album.elm .;
         ${lib.concatStrings (map (module: ''
-          elm make --warn ${elmfile module} --output $out/${module}.js --docs $out/share/doc/${module}.json
+          elm make ${elmfile module} --output $out/${module}.js --docs $out/share/doc/${module}.json --optimize
         '') targets)}
       '';
 
@@ -61,14 +45,19 @@ in mkDerivation {
   src = lib.cleanSourceWith {
     src = ./.;
     filter = path: type:
-      type == "regular" && (
+      (type == "regular" && (
         pkgs.lib.hasSuffix ".elm" path ||
-        pkgs.lib.hasSuffix "elm-package.json" path ||
+        pkgs.lib.hasSuffix "elm.json" path ||
         pkgs.lib.hasSuffix "index.html" path ||
         pkgs.lib.hasSuffix ".htaccess" path
-      );
+      )) ||
+      (type == "directory" && (
+        pkgs.lib.hasSuffix "vendor" path ||
+        pkgs.lib.hasSuffix "elm-route-url" path ||
+        pkgs.lib.hasSuffix "touch-events" path ||
+        pkgs.lib.hasSuffix "src" path
+      ));
   };
   srcdir = ".";
   targets = [ "Main" ];
 }
-
