8fb00027c385a730c7ccd28b4355097475ba0e5e
Matt McHenry <github@matt.mchenryfamily.org>
2018-12-28 23:09:55 -0500

    enable debug output

--- src/client/Utils/DebugSupport.elm
+++ src/client/Utils/DebugSupport.elm
@@ -3,11 +3,11 @@ module Utils.DebugSupport exposing (debugString, log)
 
 log : String -> a -> a
 log str val =
-    --Debug.log str val
-    val
+    --val
+    Debug.log str val
 
 
 debugString : a -> String
 debugString val =
-    --Debug.toString val
-    "Debug.toString not supported in --optimize mode"
+    --"Debug.toString not supported in --optimize mode"
+    Debug.toString val
