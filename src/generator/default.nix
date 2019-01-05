72cac7406cf3f69fce2c920124a80b4729d460f5
Matt McHenry <github@matt.mchenryfamily.org>
2019-01-04 22:47:57 -0500

    update to ghc863 and remove upper bound on base dependency

--- src/generator/default.nix
+++ src/generator/default.nix
@@ -1,2 +1,2 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc863" }:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./gen-album.nix { }
