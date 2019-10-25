66afd24372d14a1997a7b4b2410a3ae4f77a96aa
Matt McHenry <github@matt.mchenryfamily.org>
2019-10-25 19:59:50 -0400

    ghc: 8.6.4 -> 8.8.1

--- src/generator/default.nix
+++ src/generator/default.nix
@@ -1,2 +1,2 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc864" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc881" }:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./gen-album.nix { }
