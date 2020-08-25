5d1316ee106e1b7665cf5452da4bb597e67bf849
Matt McHenry <github@matt.mchenryfamily.org>
2020-08-24 20:51:01 -0400

    ghc: 8.8.3 -> 8.8.4

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc883" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc884" }:
 let
   inherit (nixpkgs) pkgs;
   ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
