770bc674c1371239c6d59f964d55ad3404bae372
Matt McHenry <github@matt.mchenryfamily.org>
2020-09-25 22:14:48 -0400

    update elm dependencies

--- src/client/nix/elm-srcs.nix
+++ src/client/nix/elm-srcs.nix
@@ -16,8 +16,8 @@
     sha256 = "0nagb9ajacxbbg985r4k9h0jadqpp0gp84nm94kcgbr5sf8i9x13";
   };
   "elm/core" = {
-    version = "1.0.4";
-    sha256 = "0gyk7lx3b6vx2jlfbxdsb4xffn0wdvg5yxldq50jr2kk5dzc2prj";
+    version = "1.0.5";
+    sha256 = "19w0iisdd66ywjayyga4kv2p1v9rxzqjaxhckp8ni6n8i0fb2dvf";
   };
   "elm/html" = {
     version = "1.0.0";
@@ -44,8 +44,8 @@
     sha256 = "0av8x5syid40sgpl5vd7pry2rq0q4pga28b4yykn9gd9v12rs3l4";
   };
   "elm-community/basics-extra" = {
-    version = "4.0.0";
-    sha256 = "02f5c0lhw6lrxdpmn09jpfhd45sbn7mr95hwrfyilx5yfrqa4z15";
+    version = "4.1.0";
+    sha256 = "0safacqk5dkcb0rdafdq1yhsch7dki26jjplbsgvfrm1j8klvjhn";
   };
   "elm-explorations/linear-algebra" = {
     version = "1.0.3";
@@ -56,16 +56,12 @@
     sha256 = "1sph37fji07dl40rvpgbnij01zg4h7ywapxar433cdh0lay26xhb";
   };
   "mpizenberg/elm-pointer-events" = {
-    version = "4.0.1";
-    sha256 = "0dfcqwbx4lpspfpq78215nsn3mxm6y3z3lfylj7kls8qvx6sbpvj";
+    version = "4.0.2";
+    sha256 = "16s14sh01g6ssabwkf2k1xdxnahnkn0s7603cg87wd0h4myg15da";
   };
   "rtfeldman/elm-css" = {
-    version = "16.0.1";
-    sha256 = "05sgz21jnnmz6m5s74515337n1aanab2zzn8jpw3bhdjc9cwwfcj";
-  };
-  "Skinney/murmur3" = {
-    version = "2.0.8";
-    sha256 = "1iiia68w8wlp2qjilyqgnzgjcxby47gla3rw5minj8i5c64zk18j";
+    version = "16.1.0";
+    sha256 = "0nxiyxyw3kw55whkpwhrcgc0dr6a8zlm2nqvsaqdw6mzkykg0ba6";
   };
   "elm/bytes" = {
     version = "1.0.8";
