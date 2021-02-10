ada277a489749581b3d6a66ba45aa9a72d88ec91
Matt McHenry <github@matt.mchenryfamily.org>
2021-02-09 20:36:38 -0500

    very basic test of tracking <img onload>

--- src/client/Sandbox/LoadingTest.elm
+++ src/client/Sandbox/LoadingTest.elm
@@ -2,7 +2,10 @@ module Sandbox.LoadingTest exposing (..)
 
 import Browser exposing (Document)
 import Browser.Navigation exposing (Key)
-import Html exposing (br, text)
+import Html exposing (br, div, img, text)
+import Html.Attributes exposing (height, src, width)
+import Html.Events exposing (on)
+import Json.Decode exposing (succeed)
 import String exposing (fromInt)
 import Url exposing (Protocol(..), Url, toString)
 import Utils.HttpUtils exposing (viewProgress)
@@ -10,7 +13,7 @@ import Utils.Loading as Loading exposing (LoadedSubstate(..), ManyModel, ManyMsg
 
 
 pathToUrl host basePath path =
-    { protocol = Http
+    { protocol = Https
     , host = host
     , port_ = Nothing
     , path = "/" ++ basePath ++ path
@@ -42,12 +45,14 @@ makeUrls host basePath =
 
 type alias Model =
     { urls : List Url
+    , loaded : List Url
     , model : ManyModel Msg
     }
 
 
 type Msg
     = Msg ManyMsg
+    | Loaded Url
     | NoOp
 
 
@@ -72,7 +77,7 @@ init url _ =
         ( lm, lc ) =
             Loading.initMany (List.take 5 urls) (List.drop 5 urls) Msg
     in
-    ( { urls = urls, model = lm }, lc )
+    ( { urls = urls, loaded = [], model = lm }, lc )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -86,49 +91,71 @@ update msg model =
                 ( newModel, newCmd ) =
                     Loading.updateMany m model.model (always model.urls)
             in
-            ( { urls = model.urls, model = newModel }, newCmd )
+            ( { model | model = newModel }, newCmd )
+
+        Loaded url ->
+            case List.member url model.loaded of
+                True ->
+                    ( model, Cmd.none )
+
+                False ->
+                    ( { model | loaded = url :: model.loaded }, Cmd.none )
 
 
 view : Model -> Document Msg
 view model =
     { title = "Loading Test"
     , body =
-        List.intersperse (br [] []) <|
-            List.map (viewOne model.model) model.urls
+        text ("loaded: " ++ (fromInt <| List.length model.loaded))
+            :: (List.intersperse (br [] []) <|
+                    List.map (viewOne model.model) model.urls
+               )
     }
 
 
 viewOne model url =
-    text <|
-        case getOneState model url of
-            Just state ->
-                toString url
-                    ++ ": "
-                    ++ (case state of
-                            Loading.NotRequested ->
-                                "not requested"
+    case getOneState model url of
+        Just state ->
+            let
+                label l =
+                    text <|
+                        toString url
+                            ++ ": "
+                            ++ l
+            in
+            case state of
+                Loading.NotRequested ->
+                    label "not requested"
 
-                            Loading.RequestedButNoProgress ->
-                                "requested, awaiting initial progress"
+                Loading.RequestedButNoProgress ->
+                    label "requested, awaiting initial progress"
 
-                            Loading.Loading progress ->
-                                viewProgress "" <| Just progress
+                Loading.Loading progress ->
+                    label <| viewProgress "" <| Just progress
 
-                            Loading.Loaded JustNow ->
-                                "just now loaded"
+                Loading.Failed _ ->
+                    label "failed"
 
-                            Loading.Loaded Recently ->
-                                "recently loaded"
+                Loading.Loaded ls ->
+                    let
+                        t =
+                            case ls of
+                                JustNow ->
+                                    "just now loaded"
 
-                            Loading.Loaded Durably ->
-                                "durably loaded"
+                                Recently ->
+                                    "recently loaded"
 
-                            Loading.Failed _ ->
-                                "failed"
-                       )
+                                Durably ->
+                                    "durably loaded"
+                    in
+                    div []
+                        [ img [ on "load" <| succeed <| Loaded url, src <| toString url, width 10, height 10 ] []
+                        , label t
+                        ]
 
-            Nothing ->
-                "mysterious missing state for " ++ toString url
+        Nothing ->
+            text <| "mysterious missing state for " ++ toString url
 
 
 subscriptions : Model -> Sub Msg
