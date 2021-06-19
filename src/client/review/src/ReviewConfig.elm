0bcbfba21c7859ca6761aaff5d5a22290810d2bd
Matt McHenry <github@matt.mchenryfamily.org>
2021-06-19 18:36:59 -0400

    elm-review init, plus some local configs

---
+++ src/client/review/src/ReviewConfig.elm
@@ -0,0 +1,42 @@
+module ReviewConfig exposing (config)
+
+{-| Do not rename the ReviewConfig module or the config function, because
+`elm-review` will look for these.
+
+To add packages that contain rules, add them to this review project using
+
+    `elm install author/packagename`
+
+when inside the directory containing this file.
+
+-}
+
+import NoUnused.CustomTypeConstructorArgs
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Exports
+import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
+import Review.Rule exposing (Rule, ignoreErrorsForDirectories, ignoreErrorsForFiles)
+
+
+config : List Rule
+config =
+    List.map
+        (ignoreErrorsForDirectories [ "vendor" ]
+            >> ignoreErrorsForFiles [ "src/Album.elm" ]
+        )
+        [ NoUnused.CustomTypeConstructors.rule []
+        , NoUnused.CustomTypeConstructorArgs.rule
+            |> ignoreErrorsForFiles [ "src/Utils/Loading.elm" ]
+        , NoUnused.Dependencies.rule
+        , NoUnused.Exports.rule
+            |> ignoreErrorsForFiles [ "src/Utils/Loading.elm" ]
+        , NoUnused.Modules.rule
+        , NoUnused.Parameters.rule
+            |> ignoreErrorsForFiles [ "src/Utils/DebugSupport.elm" ]
+        , NoUnused.Patterns.rule
+        , NoUnused.Variables.rule
+        ]
