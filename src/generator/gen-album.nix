ee2787892dbce0699209a5c74e21cb9b34c16190
Matt McHenry <github@matt.mchenryfamily.org>
2021-09-04 14:53:13 -0400

    add new haskell deps to src/generator/gen-album.nix in addition to shell.nix

--- src/generator/gen-album.nix
+++ src/generator/gen-album.nix
@@ -1,5 +1,5 @@
-{ mkDerivation, aeson, async, base, bytestring, elm-bridge
-, filepath, friday, friday-juicypixels, JuicyPixels, regex-compat, parallel
+{ mkDerivation, aeson, async, hip, base, bytestring, elm-bridge
+, filepath, friday, friday-juicypixels, JuicyPixels-extra, regex-compat, parallel, extra, safe
 , lib
 }:
 mkDerivation {
@@ -9,8 +9,8 @@ mkDerivation {
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
-    aeson async base bytestring elm-bridge filepath friday
-    friday-juicypixels JuicyPixels regex-compat parallel
+    aeson async hip base bytestring elm-bridge filepath friday
+    friday-juicypixels JuicyPixels-extra regex-compat parallel extra safe
   ];
   homepage = "http://matt.mchenryfamily.org";
   description = "a web photo album generator";
