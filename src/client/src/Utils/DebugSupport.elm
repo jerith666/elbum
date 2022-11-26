1d93328cb4d93270f3516008322007612a27f404
Matt McHenry <github@matt.mchenryfamily.org>
2022-11-26 11:07:45 -0500

    Revert "turn on debugging"
    This reverts commit 0d1c5aee52e771b4c4a0d24bd2d3530032f354b3.

--- src/client/src/Utils/DebugSupport.elm
+++ src/client/src/Utils/DebugSupport.elm
@@ -3,9 +3,9 @@ module Utils.DebugSupport exposing (debugString, log)
 
 log : String -> a -> a
 log str val =
-    Debug.log str val
+    val
 
 
 debugString : a -> String
 debugString val =
-    Debug.toString val
+    "Debug.toString not supported in --optimize mode"
