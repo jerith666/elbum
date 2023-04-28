b21ddb5a850f8b43cbacc7deac5048ccb30ece3f
Matt McHenry <github@matt.mchenryfamily.org>
2023-04-27 21:14:54 -0400

    move lamdera.nix into nix/

--- shell.nix
+++ shell.nix
@@ -12,7 +12,7 @@ let
           tasty tasty-golden
         ]);
 
-  lamdera = with nixpkgs.pkgs; import ./lamdera.nix {inherit fetchurl stdenv lib ncurses5 gmp5 zlib autoPatchelfHook;};
+  lamdera = with nixpkgs.pkgs; import ./nix/lamdera.nix {inherit fetchurl stdenv lib ncurses5 gmp5 zlib autoPatchelfHook;};
 
   # pinned to recent (but cached) ancestor of
   # dadc08be jetbrains.idea-{community,ultimate}: 2021.3.2 → 2022.1
