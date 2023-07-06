d2133412aa94eef321c8b17ac7100b51641977ec
Matt McHenry <github@matt.mchenryfamily.org>
2023-07-05 21:34:34 -0400

    niv-update: use minimal shell to reduce runtime

---
+++ nix/niv-shell.nix
@@ -0,0 +1,13 @@
+{ sources ? import ./sources.nix }:
+
+let
+  nixpkgs = import sources.nixpkgs { };
+in
+
+nixpkgs.pkgs.stdenv.mkDerivation {
+  name = "niv-env-0";
+  buildInputs = with nixpkgs.pkgs;
+    [
+      niv
+    ];
+}
